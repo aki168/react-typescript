468c7bb0739abf306de7b38b5adc4af6c2899de4
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2023-01-10 17:22:56 +0800

    init ts-react-project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import './index.css'
+import React from 'react'
+import { render } from 'react-dom'
+
+const root = document.getElementById('root')
+import App from './components/App'
+
+render(<App/>, root)
